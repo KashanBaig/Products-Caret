f31e2cfb582c6bec6373435dc714a3c2651a6d8a
izhan25 <izhan.yameen25@gmail.com>
2018-11-06 19:32:58 +0500

    migrations added

---
+++ database/migrations/2018_11_06_142839_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('Prod_id');
+            $table->timestamps();
+            $table->string('Prod_name');
+            $table->integer('Prod_price');
+            $table->unsignedInteger('Cat_id');
+
+            $table->foreign('Cat_id')->references('Cat_id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
